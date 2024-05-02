2a3940ecc2c0813e46b2484679d3b2a13c5c28f4
galiano <piz@holebois.com>
2024-05-02 13:04:36 -0500

    update empty check

--- renderer/src/routes/library/+page.svelte
+++ renderer/src/routes/library/+page.svelte
@@ -7,7 +7,7 @@
 {#await window.fictron?.getLibrary()}
     <p>Loading</p>
 {:then library} 
-    {#if library}
+    {#if library && library.length > 0}
         {#each library as fic}
             <a href="/fic/{fic.id}"><h2>{fic.name}</h2></a>
             <p>By: {fic.author_name}</p>
