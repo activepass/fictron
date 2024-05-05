ade581bc51704dd43568b41dd99ed72df4239eb9
galiano <piz@holebois.com>
2024-05-05 14:05:03 -0500

    old code removal

--- renderer/src/routes/+page.svelte
+++ renderer/src/routes/+page.svelte
@@ -1,44 +1,6 @@
 <script lang='ts'>
-	import { ao3_url, ffn_url } from "$shared/Constants";
-    import { goto } from '$app/navigation';
 	import FicPanel from "$components/FicPanel.svelte";
-	import { onMount } from "svelte";
-
-    let search: HTMLInputElement;
-
-    function go(id: number) {
-        if (id == -1) {
-            window.alert("Failed to add Fic!")
-            return;
-        }
-        window.location.href = `/fic/${id}`;
-    }
-
-    async function addUrl() {
-        if (!search || !search.value || search.value.trim() == "") return;
-        if (search.value.startsWith(ao3_url)) {
-            let num = await window.fictron?.addAo3FicToLibrary(search.value);
-            go(num!)
-        }
-        else if (search.value.startsWith(ffn_url)) {
-            let num = await window.fictron?.addFFNetFicToLibrary(search.value);
-            go(num!)
-        }
-    }
-
-    onMount(() => {
-        // search.addEventListener("keydown", (e) => {
-        //     if (e.key == "Enter") {
-        //         addUrl();
-        //     }
-        // })
-    })
 </script>
-<!-- <h1>Enter a Fic Url</h1>
-<div class="nav">
-    <input bind:this={search} class="url_input" type="text">
-    <button on:click={addUrl}>Go</button>
-</div> -->
 {#await window.fictron?.getLibraryRecent()}
     <p>Loading</p>
 {:then library} 
@@ -53,20 +15,6 @@
 {/await}
 
 <style>
-    .nav {
-        display: flex;
-        /* justify-content: center; */
-        align-items: center;
-        margin: 10px;
-    }
-    .nav input {
-        flex: 1;
-        margin-right: 10px;
-    }
-    h1 {
-        text-align: center;
-    }
-
     .panel-wrapper {
         display: flex;
         /* justify-content: center; */
